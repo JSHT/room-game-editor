af7134b2df36ca3bac57dc84f3f791eee53a13c6
JSHT <jsht.diligent.mode@gmail.com>
2022-03-04 13:49:51 +0800

    solve the advanced challenge

--- all.js
+++ all.js
@@ -1,14 +1,27 @@
+Quill.register("modules/imageUploader", ImageUploader);
+
 let myToolbar = [
   ["bold", "italic", "underline", "strike"],
   [{ align: [] }, { color: [] }, { background: [] }],
   [{ list: "ordered" }, { list: "bullet" }],
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ size: [] }],
   ["link", "image", "blockquote", "code-block"],
 ];
 
 const quill = new Quill("#description", {
   theme: "snow",
   modules: {
+    imageUploader: {
+      upload: (file) => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(
+              "https://images.unsplash.com/photo-1486427944299-d1955d23e34d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            );
+          }, 3500);
+        });
+      },
+    },
     imageResize: {
       displaySize: true,
     },
